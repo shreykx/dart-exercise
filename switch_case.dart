93ece40245770541c64d82bf791c4b6efe3dcc8f
Shrey <shreyashkumar.me@gmail.com>
2024-11-24 18:13:03 +0530

    ..

---
+++ switch_case.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  int num1 = 200;
+  switch (num1) {
+    case 100:
+      print("it is 100");
+    case 200:
+      print('it is 200');
+    default:
+      print('default case');
+  }
+}
